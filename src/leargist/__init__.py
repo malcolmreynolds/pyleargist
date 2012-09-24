62ff7fc7c0948dec3bee41f7f5e4650d74ec25cc
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2012-09-24 15:24:28 +0100

    Added BW gist.

--- src/leargist/__init__.py
+++ src/leargist/__init__.py
@@ -11,18 +11,61 @@ leargist_folder = os.path.abspath(__file__).rsplit(os.path.sep, 1)[0]
 leargist_name = "_gist"
 libleargist = np.ctypeslib.load_library(leargist_name, leargist_folder)
 
-class GistColorImage(Structure):
+class GistBwImage(Structure):
+    '''Matches image_t declared in standalone_image.h'''
     _fields_ = [
-        ("width", c_int), # stride = width
+        ("width", c_int),
         ("height", c_int),
-        ("c1", POINTER(c_float)), # R
-        ("c2", POINTER(c_float)), # G
-        ("c3", POINTER(c_float)), # B
+        ("stride", c_int),  # stride needs to be computed separately
+        ("data", POINTER(c_float))
     ]
 
+
+class GistColorImage(Structure):
+    '''Matches color_image_t declared in standalone_image.h'''
+    _fields_ = [
+        ("width", c_int),  # stride = width
+        ("height", c_int),
+        ("c1", POINTER(c_float)),  # R
+        ("c2", POINTER(c_float)),  # G
+        ("c3", POINTER(c_float)),  # B
+    ]
+
+# Setup argument & return types for color gist
 libleargist.color_gist_scaletab.argtypes = (
     POINTER(GistColorImage), c_int, c_int, POINTER(c_int))
 libleargist.color_gist_scaletab.restype = c_void_p
+
+# Setup argument & return types
+libleargist.bw_gist_scaletab.argtypes = (
+    POINTER(GistBwImage), c_int, c_int, POINTER(c_int))
+libleargist.bw_gist_scaletab.restype = c_void_p
+
+
+def bw_gist(im, nblocks=4, orientations=(8, 8, 4)):
+
+    scales = len(orientations)
+    orientations = np.array(orientations, dtype=np.int32)
+
+    if im.shape[0] < 8 or im.shape[1] < 8:
+        raise ValueError(
+            "image size must at least be (8, 8), got %s" % im.size)
+
+    im = np.ascontiguousarray(im, dtype=np.float32)
+
+    gbwi = GistBwImage(
+        im.shape[1],  # Width is the SECOND element of the shape tuple
+        im.shape[0],
+        im.ctypes.data_as(POINTER(c_float)))
+
+    # We don't need a *3 because it's black & white
+    descriptors = c_float * nblocks * nblocks * orientations.sum()
+
+    addr = libleargist.bw_gist_scaletab(
+        pointer(gbwi), nblocks, scales,
+        orientations.ctypes.data_as(POINTER(c_int)))
+    return np.ctypeslib.as_array(descriptors.from_address(addr))
+
 
 def color_gist(im, nblocks=4, orientations=(8, 8, 4)):
     """Compute the GIST descriptor of an RGB image"""
@@ -51,7 +94,7 @@ def color_gist(im, nblocks=4, orientations=(8, 8, 4)):
         arr[2].ctypes.data_as(POINTER(c_float)))
 
     descriptors = c_float * (nblocks * nblocks * orientations.sum() * 3)
-    addr= libleargist.color_gist_scaletab(
+    addr = libleargist.color_gist_scaletab(
         pointer(gci), nblocks, scales,
         orientations.ctypes.data_as(POINTER(c_int)))
     return np.ctypeslib.as_array(descriptors.from_address(addr))
